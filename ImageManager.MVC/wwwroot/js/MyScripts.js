9333fab0d1109517a1c0dad85218251d87fdc893
kotstreet <kotstreet@users.noreply.github.com>
2021-01-05 02:00:20 +0300

    A bit change js file.

--- ImageManager.MVC/wwwroot/js/MyScripts.js
+++ ImageManager.MVC/wwwroot/js/MyScripts.js
@@ -1,31 +1,28 @@
-﻿function inputFile_changed(files) {
+﻿var canvas = document.getElementById("myCanvas");
+var imageUrlInput = document.getElementById("imageUrl");
+var fileInputItem = document.getElementById("fileInputItem");
+
+function inputFile_changed(files) {
 	if (!files.length) {
 		return;
 	}
 
-	var size = files[0].size + " байт;"
-
-	var canvas = document.getElementById("myCanvas");
 	var context = canvas.getContext("2d");
-
 	var imgUrl = window.URL.createObjectURL(files[0])
 	var img = new Image();
+
 	img.onload = function () {
 		canvas.width = this.width;
 		canvas.height = this.height;
+		canvas.title = "ширина: " + this.width + "; высота: " + this.height + "; " + files[0].size + " байт;";
 
 		context.drawImage(img, 0, 0, this.width, this.height);
-		canvas.title = "ширина: " + this.width + "; высота: " + this.height + "; " + size;
 	};
 
-	var imageUrlInput = document.getElementById("imageUrl");
 	imageUrlInput.value = imgUrl;
-
 	img.src = imgUrl;
 }
 
-
 function open_click() {
-	var fileInputItem = document.getElementById("fileInputItem");
 	fileInputItem.click();
 }
